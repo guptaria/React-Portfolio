5c8ff274150f9726698bafb4fe751edf78b84831
Priya Garg <gupta.ria100@gmail.com>
2021-03-19 00:42:12 -0500

    adding nav bar and image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "../src/components/Header/index";
+import Data from "../src/utils/Data";
 
 function App() {
   return (
-   
+   <Header Data={Data} />
   );
 }
 
