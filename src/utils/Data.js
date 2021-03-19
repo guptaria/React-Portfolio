ecfb43a016e18d3afb81bd9ec30f488da18e74b2
Priya Garg <gupta.ria100@gmail.com>
2021-03-18 23:42:58 -0500

    created data json file

---
+++ src/utils/Data.js
@@ -0,0 +1,109 @@
+let Data = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Priya Garg",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"www.linkedin.com/in/priyagarg123",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"www.linkedin.com/in/priyagarg123",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/guptaria",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I’m a certified Full Stack Web Developer (University of Texas in Austin) who is very passionateabout learning new things. I’m a constant learner who’s continually looking for ways to help advance my knowledge and career. I’m enthusiastic about building creative and user-friendly applications. I’m proud to say I have always left a positive mark in my past workplaces due to my ability to build good relationship with my coworkers. I love Painting and Travel.Proactive in learning new skills and knowledge with core competency in full stack web development such as Javascript, jQuery, HTML, CSS, MERN Stack, SQL, REST API’s.",
+
+    "address":"San Antonio,Tx",
+    "website":"https://guptaria.github.io/updated_portfolio/",
+    "education":[
+      {
+        "UniversityName":"University of Texas at Austin",
+        "specialization":"Full Stack",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2021",
+        // "Achievements":"Some Achievements"
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"My Skills ",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Travel Journal",
+        "description":"Social Media Network to allow for creation of Trip Journals to share with family and friends!",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Eat-Da-Burger",
+        "description":"This project is a full stack application that allows you to place an order for a burger. When you place an order, the burger you enter in the form field is added to the list of burgers on the left side of the screen",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Weather_Dashboard",
+        "description":"The weather app will be used to: Get current weather information like temperature, pressure, humidity, wind speed etc. Get weather forecast information of any city by city name ",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"Daily Planner",
+        "description":"This project provides users with a daily planner where they can add their own events to an hourly log for the business hours of 9am-5pm.",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default Data;
